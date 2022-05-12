afd2bd637a14eabf96230098f2be987d97d2ae30
xu.jin <xyjy247@gmail.com>
2022-05-12 10:24:11 +0800

    6

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
+    long_description=open("README.md").read(),
+
     name='Gollum',
     version='0.1',
     author="bitex47",
     author_email="xyjy5247@gmail.com",
     packages=find_packages(),
     python_requires='>=3.6',
-    license="MIT",
+    # license="MIT",
     install_requires=[
         'paramiko >= 2.10.1',
     ],
