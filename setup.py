a04a1b3a28af33fff1f4b48913ed83f0ada2a0f6
xu.jin <xyjy247@gmail.com>
2022-05-12 10:17:57 +0800

    5

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email="xyjy5247@gmail.com",
     packages=find_packages(),
     python_requires='>=3.6',
+    license="MIT",
     install_requires=[
         'paramiko >= 2.10.1',
     ],
